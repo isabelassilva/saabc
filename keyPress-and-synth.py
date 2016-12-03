6ece72ce2e96c9f15fe45eab9017cd7070415eea
isabelassilva <isabelassilva@gmail.com>
2016-12-03 08:54:17 -0400

    Recognize and Read each key from keyboard with Tkinter and Pttsx

---
+++ keyPress-and-synth.py
@@ -0,0 +1,24 @@
+import pyttsx
+
+from Tkinter import *
+
+def key(event):
+    engine = pyttsx.init()
+    if event.char == event.keysym:
+        msg = 'Normal Key %r' % event.char
+    elif len(event.char) == 1:
+        msg = 'Punctuation Key %r (%r)' % (event.keysym, event.char)
+    else:
+        msg = 'Special Key %r' % event.keysym
+    label1.config(text=msg)
+    engine.say(msg)
+    engine.runAndWait()
+
+root = Tk()
+prompt = '      Press any key      '
+label1 = Label(root, text=prompt, width=len(prompt), bg='yellow')
+label1.pack()
+
+root.bind_all('<Key>', key)
+
+root.mainloop()
