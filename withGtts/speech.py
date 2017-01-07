11e87248bafb146bdd59ba69480073ede413bd22
isabelassilva <isabelassilva@gmail.com>
2017-01-07 13:46:03 -0400

    Play mp3 file with Pygame Lib

--- withGtts/speech.py
+++ withGtts/speech.py
@@ -3,3 +3,13 @@ from gtts import gTTS
 tts = gTTS(text='Hello', lang='en')
 
 tts.save("hello.mp3")
+
+from Tkinter import *
+root = Tk()
+
+from pygame import mixer
+mixer.init()
+mixer.music.load('hello.mp3')
+mixer.music.play()
+
+root.mainloop()
