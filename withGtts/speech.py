4511763ee187a5858e34b00719c717aa5ebc19c3
isabelassilva <isabelassilva@gmail.com>
2017-01-26 11:18:06 -0400

    Play mp3 file with WebBrowser Lib

--- withGtts/speech.py
+++ withGtts/speech.py
@@ -4,17 +4,20 @@ tts = gTTS(text='Hello', lang='en')
 
 tts.save("hello.mp3")
 
-from Tkinter import *
-root = Tk()
+#from Tkinter import *
+#root = Tk()
 
 """
 from pygame import mixer
 mixer.init()
 mixer.music.load('hello.mp3')
 mixer.music.play()
-"""
+
 import vlc
 p = vlc.MediaPlayer("hello.mp3")
 p.play()
+"""
+import webbrowser
+webbrowser.open("hello.mp3")
 
-root.mainloop()
+#root.mainloop()
